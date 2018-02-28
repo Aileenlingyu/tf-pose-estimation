f48ea5dd998834db1fb75c2e810740dc1f81ab47
zaikun_xu <zaikun.xu@advertima.com>
2018-02-28 12:58:17 +0100

    mobilenet_V2 works

--- src/networks.py
+++ src/networks.py
@@ -31,9 +31,10 @@ def get_network(type, placeholder_input, sess_for_load=None, trainable=True):
         net = MobilenetNetworkThin({'image': placeholder_input}, conv_width=0.75, conv_width2=0.50, trainable=trainable)
         pretrain_path = 'pretrained/mobilenet_v1_0.75_224_2017_06_14/mobilenet_v1_0.75_224.ckpt'
         last_layer = 'MConv_Stage6_L{aux}_5'
+
     elif type == 'mobilenet_v2':
         net = MobilenetV2({'image': placeholder_input},  trainable=trainable)
-        pretrain_path = ''
+        pretrain_path = 'numpy/mobilenet_v2.npy'
         last_layer = 'MConv_Stage6_L{aux}_5'
 
     elif type == 'cmu':
