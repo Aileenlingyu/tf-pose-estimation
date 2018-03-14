03fd8cb3ab58160e9fcffa08a3527ce6d216b445
zaikun <zaikunxu1989@gmail.com>
2018-03-14 09:41:54 +0100

    mobilenet multi-scale multi-stage

--- src/networks.py
+++ src/networks.py
@@ -5,6 +5,7 @@ from network_mobilenet_thin import MobilenetNetworkThin
 from network_mobilenet_thin_fatbranch import MobilenetNetworkThinFatBranch
 from network_mobilenet_zaikun import MobilenetNetworkZaikun
 from network_mobilenet_zaikun_side import MobilenetNetworkZaikunSide
+from network_mobilenet_ms import MobilenetNetworkZaikunMs
 
 from network_mobilenet_thin_upsampling import MobilenetNetworkThinUp
 from network_mobilenet_thin_dilate import MobilenetNetworkThinDilate
@@ -60,10 +61,10 @@ def get_network(type, placeholder_input, sess_for_load=None, trainable=True):
         pretrain_path = 'pretrained/mobilenet_v1_0.75_224_2017_06_14/mobilenet_v1_0.75_224.ckpt'
         last_layer = 'MConv_Stage4_L{aux}_5'
 
-    elif type == 'mobilenet_zaikun_side':
+    elif type == 'mobilenet_ms':
         net = MobilenetNetworkZaikunSide({'image': placeholder_input}, conv_width=0.75, conv_width2=0.50, trainable=trainable)
         pretrain_path = 'pretrained/mobilenet_v1_0.75_224_2017_06_14/mobilenet_v1_0.75_224.ckpt'
-        last_layer = 'MConv_Stage2_L{aux}_5'
+        last_layer = 'MConv_Stage4_L{aux}_5'
 
     elif type == 'mobilenet_thin_fatbranch':
         net = MobilenetNetworkThinFatBranch({'image': placeholder_input}, conv_width=0.75, conv_width2=0.50, trainable=trainable)
@@ -80,7 +81,6 @@ def get_network(type, placeholder_input, sess_for_load=None, trainable=True):
         net = MobilenetNetworkThinDilate({'image': placeholder_input}, conv_width=0.75, conv_width2=0.50, trainable=trainable)
         pretrain_path = 'pretrained/mobilenet_v1_0.75_224_2017_06_14/mobilenet_v1_0.75_224.ckpt'
         last_layer = 'MConv_Stage6_L{aux}_5'
-
 
     elif type == 'mobilenet_v2':
         net = MobilenetNetworkV2All({'image': placeholder_input}, conv_width=1, conv_width2=0.50, trainable=trainable)
@@ -124,6 +124,7 @@ def get_network(type, placeholder_input, sess_for_load=None, trainable=True):
                 'mobilenet_thin': 'trained/mobilenet_thin_432x368_stride5/model-26000',
                 'mobilenet_fast': 'trained/mobilenet_fast_%s/model-189000' % s,
                 'mobilenet_accurate': 'trained/mobilenet_accurate/model-170000',
+                'mobilenet_zaikun_side' : 'trained/mobilenet_zaikun_side/model-48000',
                 'vgg16x4' : 'trained/vgg16x4_0.75/model-35000',
                 'vgg16x5': 'trained/vgg16x5/model-32000',
                 'vgg': 'trained/vgg/model-31000'
