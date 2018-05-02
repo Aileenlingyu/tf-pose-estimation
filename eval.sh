d0e0c0f96a849f98be773307e9897f497975b438
zaikun <zaikunxu1989@gmail.com>
2018-05-02 14:02:35 +0200

    update eval.sh

--- eval.sh
+++ eval.sh
@@ -5,8 +5,10 @@ do
 	python3 src/eval.py --model=$model  --input-width=656 --input-height=368
 	python3 src/eval.py --model=$model  --input-width=440 --input-height=256
 	python3 src/eval.py --model=$model  --input-width=328 --input-height=184 
+	python3 src/eval.py --model=$model  --input-width=800 --input-height=600
 done
 
-#CUDA_VISIBLE_DEVICES=0 python3 src/eval_caffemodel.py  --input-width=656 --input-height=368
-#CUDA_VISIBLE_DEVICES=0 python3 src/eval_caffemodel.py  --input-width=440 --input-height=256
-#CUDA_VISIBLE_DEVICES=0 python3 src/eval_caffemodel.py  --input-width=328 --input-height=184
+CUDA_VISIBLE_DEVICES=0 python3 src/eval_caffemodel.py  --input-width=656 --input-height=368
+CUDA_VISIBLE_DEVICES=0 python3 src/eval_caffemodel.py  --input-width=440 --input-height=256
+CUDA_VISIBLE_DEVICES=0 python3 src/eval_caffemodel.py  --input-width=328 --input-height=184
+CUDA_VISIBLE_DEVICES=0 python3 src/eval_caffemodel.py  --input-width=800 --input-height=600
