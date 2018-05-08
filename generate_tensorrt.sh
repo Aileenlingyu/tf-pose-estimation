d36e68bf670d90b77c9d0ed7ad261d580f052305
zaikun <zaikunxu1989@gmail.com>
2018-05-08 10:08:12 +0200

    update half16

--- generate_tensorrt.sh
+++ generate_tensorrt.sh
@@ -1,13 +1,38 @@
 
-python3 src/inference.py --model=mobilenet_thin --input-width=$1 --input-height=$2
 
+python3 src/inference.py --model=mobilenet_thin --input-width=800 --input-height=540
 python3 -m tensorflow.python.tools.freeze_graph --input_graph graph-tmp.pb --output_graph graph_freeze.pb --input_checkpoint ./models/trained/mobilenet_thin_432x368/model-160001 --output_node_names=Openpose/concat_stage7
+python3 src/tensorrt_inference.py --input-width=800 --input-height=540 --graph=graph_freeze.pb --engine=mobilenet_thin_800x540_half16.engine --half16=1
 
-# $HOME/hdd/tensorflow/bazel-bin/tensorflow/tools/graph_transforms/transform_graph     --in_graph=graph_freeze.pb    --out_graph=graph_opt.pb    --inputs='image'     --outputs='Openpose/concat_stage7'     --transforms='
-#     strip_unused_nodes(type=float, shape="1,$2,$1,3")
-#     remove_nodes(op=Identity, op=CheckNumerics)
-#     fold_constants(ignoreError=False)
-#     fold_old_batch_norms
-#     fold_batch_norms'
+python3 src/inference.py --model=mobilenet_thin --input-width=656 --input-height=368
+python3 -m tensorflow.python.tools.freeze_graph --input_graph graph-tmp.pb --output_graph graph_freeze.pb --input_checkpoint ./models/trained/mobilenet_thin_432x368/model-160001 --output_node_names=Openpose/concat_stage7
+python3 src/tensorrt_inference.py --input-width=656 --input-height=$368 --graph=graph_freeze.pb --engine=mobilenet_thin_656x368_half16.engine --half16=1
 
-python3 src/tensorrt_inference.py --input-width=$1 --input-height=$2 --graph=graph_freeze.pb --engine=mobilenet_thin_$1x$2_half16_$3.engine
+python3 src/inference.py --model=mobilenet_thin --input-width=440 --input-height=256
+python3 -m tensorflow.python.tools.freeze_graph --input_graph graph-tmp.pb --output_graph graph_freeze.pb --input_checkpoint ./models/trained/mobilenet_thin_432x368/model-160001 --output_node_names=Openpose/concat_stage7
+python3 src/tensorrt_inference.py --input-width=440 --input-height=256 --graph=graph_freeze.pb --engine=mobilenet_thin_440x256_half16.engine --half16=1
+
+python3 src/inference.py --model=mobilenet_thin --input-width=328 --input-height=184
+python3 -m tensorflow.python.tools.freeze_graph --input_graph graph-tmp.pb --output_graph graph_freeze.pb --input_checkpoint ./models/trained/mobilenet_thin_432x368/model-160001 --output_node_names=Openpose/concat_stage7
+python3 src/tensorrt_inference.py --input-width=328 --input-height=184 --graph=graph_freeze.pb --engine=mobilenet_thin_328x184_half16.engine --half16=1
+
+
+
+
+python3 src/inference.py --model=mobilenet_original  --input-width=800 --input-height=540
+python3 -m tensorflow.python.tools.freeze_graph --input_graph graph-tmp.pb --output_graph graph_freeze.pb --input_checkpoint ./models/trained/mobilenet_thin_benchmark/model-388003 --output_node_names=Openpose/concat_stage7
+python3 src/tensorrt_inference.py --input-width=800 --input-height=540 --graph=./models/graph/mobilenet_thin_432x368/graph_opt.pb --engine=mobilenet_original_800x540_half16.engine  --half16=1
+
+python3 src/inference.py --model=mobilenet_original --input-width=656 --input-height=368
+python3 -m tensorflow.python.tools.freeze_graph --input_graph graph-tmp.pb --output_graph graph_freeze.pb --input_checkpoint ./models/trained/mobilenet_thin_benchmark/model-388003 --output_node_names=Openpose/concat_stage7
+python3 src/tensorrt_inference.py --input-width=656 --input-height=368 --graph=graph_freeze.pb --engine=mobilenet_thin_656x368_half16.engine  --half16=1
+
+python3 src/inference.py --model=mobilenet_original --input-width=440 --input-height=256
+python3 -m tensorflow.python.tools.freeze_graph --input_graph graph-tmp.pb --output_graph graph_freeze.pb --input_checkpoint ./models/trained/mobilenet_thin_benchmark/model-388003 --output_node_names=Openpose/concat_stage7
+python3 src/tensorrt_inference.py --input-width=440 --input-height=256 --graph=graph_freeze.pb --engine=mobilenet_thin_440x256_half16.engine  --half16=1
+
+python3 src/inference.py --model=mobilenet_original --input-width=328 --input-height=184
+python3 -m tensorflow.python.tools.freeze_graph --input_graph graph-tmp.pb --output_graph graph_freeze.pb --input_checkpoint ./models/trained/mobilenet_thin_benchmark/model-388003 --output_node_names=Openpose/concat_stage7
+python3 src/tensorrt_inference.py --input-width=328 --input-height=184 --graph=graph_freeze.pb --engine=mobilenet_thin_328x184_half16.engine  --half16=1
+
+
